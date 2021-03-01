b0547bb6adc09b2be1b2e228e5e941b1fc19c324
Bhavek Budhia <bhavek.budhia@boldcommerce.com>
2021-03-01 12:40:16 -0600

    Added instructions on how to upload to PyPi
    * Added a docs/upload-to-pypi.md file
    * Added the setup.cfg file
    * Updated setup.py: Added version, license and url meta information

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
+import pathlib
+
+
+requirements_data = pathlib.Path('.', 'requirements.txt').read_text().split('\n')
+readme = pathlib.Path('.', 'README.md').read_text()
 
 setup(
-    name='Composer Version Manager',
+    name='composer version manager',
     packages=find_packages(),
+    version='1.0.7',
+    license='MIT',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/composer-version-manager/cvm',
     entry_points={
         'console_scripts': [
             'cvm=cvm.cli:main',
         ],
     },
-    python_requires='>=3'
+    install_requires = requirements_data,
+    python_requires='>=3',
 )
